2caa3d58ac8143d44e9089ce9e3aef3d57053169
nuex001 <edeemmanuelchizurumoke@gmail.com>
2024-05-21 09:52:35 +0100

    added medium

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub, FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaMedium, FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 function Footer() {
@@ -10,6 +10,7 @@ function Footer() {
         <li><a href="https://twitter.com/nuelyoungtech" target='_blank'><FaXTwitter/></a></li>
         <li><a href="https://www.linkedin.com/in/emmanuel-ede-a26a862b4" target='_blank'><FaLinkedinIn/></a></li>
         <li><a href="https://github.com/nuex001" target='_blank'><FaGithub/></a></li>
+        <li><a href="https://medium.com/@edeemmanuelchizurumoke" target='_blank'><FaMedium /></a></li>
     </ul>
  </footer>
   )
