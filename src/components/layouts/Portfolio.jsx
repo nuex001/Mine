78b977eca5ee4d367d70dc3fe7c19aabde1b4b1c
nuex001 <edeemmanuelchizurumoke@gmail.com>
2025-04-06 21:38:49 +0100

    changed the alphablocks link in the portfolio

--- src/components/layouts/Portfolio.jsx
+++ src/components/layouts/Portfolio.jsx
@@ -72,7 +72,7 @@ function Portfolio() {
       text: "Get started with Web3 for Free, Whether you're new to the world of technology or looking to expand your knowledge in web 3, our community offers comprehensive resources to help you succeed.Start your learning adventure today!",
       subTopic: "Alphablock(Smart contract developer)",
       link: "Invprj",
-      external_link: "https://www.youtube.com/@nuelyoungtech",
+      external_link: "https://www.alphablocks.tech/",
       date: "2024",
     },
     {
