fe40bc01d37cd2b125e371335f7befadf032dd4a
nuex001 <edeemmanuelchizurumoke@gmail.com>
2025-12-14 19:51:48 +0100

    Refactored the subtopic

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,7 @@ function App() {
       <ScrollToTop/>
       <Routes>
         <Route exact path="/*" element={<Home/>}/>
-        {/* <Route exact path="/project/:id" element={<Project/>}/>
-        <Route exact path="/Daoprj" element={<Daoprj/>}/>
-        <Route exact path="/memes" element={<Memes/>}/>
-        <Route exact path="/Etherscanprj" element={<EtherscanPrj/>}/>
-        <Route exact path="/Invprj" element={<Invprj/>}/>
-        <Route exact path="/projects" element={<Projects/>}/> */}
-        {/* 100 daily projects */}
-        {/* <Route exact path="/ratonsol" element={<Ratonsol/>}/>
-        <Route exact path="/faucet" element={<Fauctnet/>}/>
-        <Route exact path="/tgbot" element={<TgBot/>}/>
-        <Route exact path="/quiz" element={<Quizapp/>}/> */}
+       
       </Routes>
       <Footer/>
     </div>
